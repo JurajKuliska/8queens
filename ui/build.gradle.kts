f8cbf05620a415e8b38fd57927c31ccb92b520fe
Juraj Kuliska <juraj.kuliska@gmail.com>
2025-06-07 02:46:38 +0200

    Clean-up build.gradle files

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -8,23 +8,6 @@ android {
     namespace = "com.jurajkuliska.eightqueens.ui"
     compileSdk = 35
 
-    defaultConfig {
-        minSdk = 24
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
     kotlinOptions {
         jvmTarget = "11"
     }
